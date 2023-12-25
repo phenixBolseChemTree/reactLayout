786c7715c8966ee8fdc2831b1e45ff947693e33e
Ivan Sharapov <oldrum75@gmail.com>
2023-12-25 19:00:51 +0100

    feat: header add min-width: 900px

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import logo from '../../assets/images/header/logo.svg'
 import user from '../../assets/images/header/user.svg'
 import bag from '../../assets/images/header/bag.svg'
 import sort from '../../assets/images/header/sort.svg'
 import search from '../../assets/images/header/search.svg'
+import down from '../../assets/images/header/down.svg'
 
 import styles from './header.module.css'
 
@@ -12,21 +14,25 @@ return (
     <div className={styles.line_1}>
       <img className={styles.logo} src={logo} alt="" />
       <div className={styles.pages}>
+        <button className={styles.helpBtn}>Help</button>
         <a href='#' className={styles.headerLink}>
           <img className={styles.headerIcon} src={user} alt="" />
+          <p className={styles.pagesText}>Account</p>
         </a>
         <a href='#' className={styles.headerLink}>
           <img className={styles.headerIcon} src={bag} alt="" />
+          <p className={styles.pagesText} >Shoping</p>
           <span className={styles.counter}>16</span>
         </a>
       </div>
     </div>
     <div className={styles.line_2}>
-      <button className={styles.btnBurger}><img className={styles.imgSort} src={sort} alt="" /></button>
+      <button className={styles.burgerBtn}><img className={styles.imgSort} src={sort} alt="" /></button>
       <div className={styles.search}>
+        <div className={styles.categories}><button className={styles.categoriesBtn}>All categories</button><img src={down} alt="" /></div>
         <input className={styles.searchInput} placeholder='Search anything' type="text" />
         <button className={styles.searchBtn} type='button'>
-          <img className={styles.searchImg} src={search} alt="" />
+          <img className={styles.searchDown} className={styles.searchImg} src={search} alt="" />
           </button>
       </div>
     </div>
