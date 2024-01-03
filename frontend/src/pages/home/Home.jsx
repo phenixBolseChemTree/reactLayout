e24b7e1537d5e7d76a671a8cc6ae8ac6d681b508
Ivan Sharapov <oldrum75@gmail.com>
2024-01-03 15:19:57 +0100

    fix: edit render

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../components/header';
+import Header from '../../components/Header';
 import HeroCategories from '../../components/heroCategories';
 import HeroProducts from '../../components/heroProducts';
 
