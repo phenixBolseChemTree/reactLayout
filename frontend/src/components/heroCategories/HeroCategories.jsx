9dacd00115972ed5cf5f14e67e350f129d83d21a
Ivan Sharapov <oldrum75@gmail.com>
2024-01-03 15:05:40 +0100

    fix: edit for deploy

--- frontend/src/components/heroCategories/HeroCategories.jsx
+++ frontend/src/components/heroCategories/HeroCategories.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './hero-categories.module.css';
-import Category from './category';
+import Category from './Category/Category';
 
 function HeroCategories({ heroCategories }) {
   return (
